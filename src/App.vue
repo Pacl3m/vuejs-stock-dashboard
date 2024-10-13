a7527e12f5c39d8ef102e6ac3fbacf5cee06c0cd
paclem <pascal.moeller@posteo.de>
2024-10-13 16:30:37 +0200

    created first component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-<div></div>
+    <BaseCard>
+      <h1>Überschrift</h1>
+      <p>Dies ist ein Beispieltest, der in der Card-Componente eingefüft wird.</p>
+    </BaseCard>
 </template>
 
 <script>
+import BaseCard from './components/BaseCard.vue';
 
 export default {
   name: 'App',
   components: {
+    BaseCard,
   }
 }
 </script>
@@ -15,9 +20,12 @@ export default {
 body {
   margin: 0;
 }
+
 #app {
   background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
+  padding: 100px;
+  box-sizing: border-box;
 }
 </style>
